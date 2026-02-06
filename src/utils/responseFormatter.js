fbc805f6eca9bc0de72b6c484fbc0902700970df
Rasmus Westerlund <rasmus.westerlund03@gmail.com>
2026-02-05 20:22:54 -0600

    Add success and error response helpers

---
+++ src/utils/responseFormatter.js
@@ -0,0 +1,27 @@
+// Format successful API responses
+function successResponse(res, data, message = 'Success', statusCode = 200) {
+    return res.status(statusCode).json({ 
+        success: true,
+        message,
+        data 
+    });
+};
+
+// Format error API responses
+function errorResponse(res, message = 'An error occurred', statusCode = 500, errors = null) {
+    const response = {
+        success: false,
+        message
+    };
+
+    if (errors) {
+        response.errors = errors;
+    }
+
+    return res.status(statusCode).json(response);
+};
+
+module.exports = {
+    successResponse,
+    errorResponse
+};
