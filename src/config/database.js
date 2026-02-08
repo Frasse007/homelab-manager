9db442bec4e68fb0298db05146df7d13bd24adb5
Rasmus Westerlund <rasmus.westerlund03@gmail.com>
2026-02-07 22:04:30 -0600

    Refactor to have separate config and credentials for production db

--- src/config/database.js
+++ src/config/database.js
@@ -32,11 +32,11 @@ module.exports = {
         }
     },
     production: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT || 5432,
+        username: process.env.PROD_DB_USER,
+        password: process.env.PROD_DB_PASSWORD,
+        database: process.env.PROD_DB_NAME,
+        host: process.env.PROD_DB_HOST,
+        port: process.env.PROD_DB_PORT || 5432,
         dialect: 'postgres',
         logging: false,
         define: {
